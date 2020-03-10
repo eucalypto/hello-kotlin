700049d0b86385b8dc29d2f3edff9160db9ff117
Viktor Papara <eucalyptov@gmail.com>
2020-03-10 20:20:18 +0100

    Add fortune cookie delivery

---
+++ src/FortuneCookie.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+    println(getFortuneCookie())
+
+    var fortune: String
+
+    for (i in 1..10) {
+        fortune = getFortuneCookie()
+        println(fortune)
+        if (fortune.contains("Take it easy")) break
+    }
+
+}
+
+fun getFortuneCookie(): String {
+    val fortunes = listOf(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
+    )
+    print("Enter your birthday: ")
+    val birthday: Int = readLine()?.toIntOrNull() ?: 1
+
+    return fortunes[birthday.rem(fortunes.size)]
+}
+
